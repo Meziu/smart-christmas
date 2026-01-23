127a44c533695b816d24cb91ee9c9b65943199f5
Andrea Ciliberti <meziu210@icloud.com>
2026-01-23 12:47:35 +0100

    Added utils functions module

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import utime
+
+
+# Helper per il fuso orario italiano (posto che funzioni la sincronizzazione NTP)
+def localtime_italy():
+    t = utime.localtime()
+    return utime.localtime(utime.mktime(t) + 3600)
+
+
+def localtime_italy_str():
+    t = localtime_italy()
+    return "{:04d}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}".format(
+        t[0], t[1], t[2], t[3], t[4], t[5]
+    )
+
+
+# Helper per ottenere le dimensioni sullo schermo del testo.
+def text_width(text):
+    return len(text) * 8
