e4a0a528b0f08e7d7133385ba842968a9c69bdc1
Andrea Ciliberti <meziu210@icloud.com>
2026-01-27 17:49:29 +0100

    Final release

--- src/smartchristmas.py
+++ src/smartchristmas.py
@@ -22,19 +22,11 @@ class SmartChristmas:
 
         self.display.clear()
         self.display.draw_header()
-        # self.display.draw_stats_page()
-        # self.display.draw_music_page()
         self.display.set_page(self.display.PAGE_STATS)
 
         self.sensors.tree_lights.on()
-        # self.sensors._buzzer.play_jb()
-        # self.sensors.buzzer.play_wwmc()
-        # self.sensors.buzzer.play_lis()
 
         while True:
             self.display.update_page()
-
-            # self.display.draw_stats_data(data)
-            # self.display.draw_music_update()
 
             self.display.oled.show()
